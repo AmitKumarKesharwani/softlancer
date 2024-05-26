60d24c79a0a98a906a4b124c233b9011e5fdea2f
Suraj Yadav <surajyadav190467@gmail.com>
2024-05-26 11:10:55 +0530

    Adding Admin Pannel side.

--- softlancer/routes/web.php
+++ softlancer/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AdminController::class)->prefix('admin')->group(function () {
+    Route::get('', 'index')->name('admin.index');
+
+    Route::post('', 'store')->name('admin.store');
 });
